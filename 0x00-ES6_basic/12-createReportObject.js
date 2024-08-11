2273fdaba0afaabfce9a17b75998f6c58834e2da
ila36IX <jbariali002@gmail.com>
2024-08-11 16:39:06 +0100

    ES6 is amazing

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,6 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: (emps = employeesList) => Object.keys(emps).length,
+  };
+}
