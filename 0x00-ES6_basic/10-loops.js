29c784f98d69fcf758cce6c27e5b1e1c0d4748d3
ila36IX <jbariali002@gmail.com>
2024-08-12 17:46:35 +0100

    fixing line spesifications

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
   for (const value of array) {
+    // eslint-disable-next-line no-param-reassign
     array[i] = appendString + value;
     i += 1;
   }
