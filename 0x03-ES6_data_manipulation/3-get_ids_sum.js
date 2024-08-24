d86aaee7e342533544c32a46a7cba3d5668b8a85
ila36IX <jbariali002@gmail.com>
2024-08-24 22:02:22 +0100

    Fix getStudent to return

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-  students.reduce((accumulator, item) => accumulator + item.id, 0);
+  return students.reduce((accumulator, item) => accumulator + item.id, 0);
 }
