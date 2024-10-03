f9f7a681c4adc4befc3d4591467e3a7f9cab4d82
ila36IX <jbariali002@gmail.com>
2024-10-03 17:01:53 +0100

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,13 +4,10 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
 
 module.exports = app;
