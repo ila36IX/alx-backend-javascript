ac8dac162cce1a90ee8cc14e71990bfd3266cc0f
ila36IX <jbariali002@gmail.com>
2024-11-01 02:28:58 +0100

    asic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,12 @@
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({ data: 'Successful response from the API' });
+  }
+});
+module.exports = getPaymentTokenFromAPI;
+// const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+//   if (success) {
+//       resolve({data: 'Successful response from the API'});
+//   }
+// });
+// module.exports = getPaymentTokenFromAPI;
